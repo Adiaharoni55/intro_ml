49cdc9ab69f043ed3d09f60ed40f1e7beda9c3c4
Adi Ahaorni <adiaharoni55@gmail.com>
2024-11-26 14:04:03 +0200

    until q4

--- files.py
+++ files.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib
 import streamlit
 
-
 def load_data(filepath: str) -> pd.DataFrame:
     """
     We assume the file will be an  Excel/csv type.
@@ -25,4 +24,23 @@ def group_and_aggregate_data(df: pd.DataFrame, group_by_column: str, agg_func) -
     :param agg_func: The aggregation function to apply to each group.
     :return: A pandas DataFrame with aggregated results.
     """
+
+    return df.groupby(group_by_column).agg(agg_func)
+
+
+def remove_sparse_columns(df: pd.DataFrame, threshold: int) -> pd.DataFrame:
+    """
+    Passing through only the numeric columns (city name will stay)
+    :param df: A pandas DataFrame.
+    :param threshold: The minimum total sum for a column to be retained in the DataFrame.
+    :return: A pandas DataFrame with sparse columns removed.
+    """
+    res = df
+    for column_name in df.select_dtypes(include=['number']).columns:
+        if res[column_name].sum() < threshold:
+            res = res.drop(columns=column_name)
+    return res
+
+
+def dimensionality_reduction(df: pd.DataFrame, num_components: int, meta_columns: list[str]) -> pd.DataFrame:
     pass
