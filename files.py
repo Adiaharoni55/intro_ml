3844a5fd5e52516174681d701617fa79abc14bda
Adi Ahaorni <adiaharoni55@gmail.com>
2024-11-26 12:07:13 +0200

    load_data demonstration

--- files.py
+++ files.py
@@ -7,9 +7,9 @@ import streamlit
 def load_data(filepath: str) -> pd.DataFrame:
     """
     We assume the file will be an  Excel/csv type.
-    To read Excel file make sure
+    To read Excel file make sure 'openpyxl' has been installed in the project.
     :param filepath: a path to an Excel/csv file
-    :return: dataframe of the file 'openpyxl' has been installed in the project.
+    :return: dataframe of the file
     """
     path_split = filepath.lower().split('.')
     if path_split[-1] == 'csv':
@@ -17,10 +17,12 @@ def load_data(filepath: str) -> pd.DataFrame:
     else:
         return pd.read_excel(filepath)
 
-# check:
-check = load_data("knesset_25.xlsx")
-print(type(check))
-print(check['city_name'])
 
 def group_and_aggregate_data(df: pd.DataFrame, group_by_column: str, agg_func) -> pd.DataFrame:
+    """
+    :param df: A pandas DataFrame containing the dataset.
+    :param group_by_column: The column to group data by.
+    :param agg_func: The aggregation function to apply to each group.
+    :return: A pandas DataFrame with aggregated results.
+    """
     pass
